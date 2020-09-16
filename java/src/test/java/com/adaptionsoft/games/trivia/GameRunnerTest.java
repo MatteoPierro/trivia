012242dde8fd36557f57e191f10cc8226802c204
Matteo Pierro <pierromatteo@gmail.com>
2020-09-16 12:20:14 +0300

    control random seed

--- java/src/test/java/com/adaptionsoft/games/trivia/GameRunnerTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/GameRunnerTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Random;
 
 public class GameRunnerTest {
 
@@ -13,11 +14,10 @@ public class GameRunnerTest {
 	public void run_a_game() throws Exception {
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		PrintStream printStream = new PrintStream(outputStream, true);
-
 		PrintStream oldOut = System.out;
 		System.setOut(printStream);
 
-		GameRunner.main(null);
+		GameRunner.run(new Random(1));
 
 		System.setOut(oldOut);
 		Approvals.verify(outputStream.toString());
