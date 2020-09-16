4370003a7861718d5e7ddc9f77f71cc6e0601af8
Matteo Pierro <pierromatteo@gmail.com>
2020-09-16 12:33:00 +0300

    try with different seeds

--- java/src/test/java/com/adaptionsoft/games/trivia/GameRunnerTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/GameRunnerTest.java
@@ -12,14 +12,19 @@ public class GameRunnerTest {
 
 	@Test
 	public void run_a_game() throws Exception {
+		Integer[] seeds = {1,2};
+		Approvals.verifyAll(seeds,seed -> runGame(seed));
+	}
+
+	private String runGame(int seed) {
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		PrintStream printStream = new PrintStream(outputStream, true);
 		PrintStream oldOut = System.out;
 		System.setOut(printStream);
 
-		GameRunner.run(new Random(1));
+		GameRunner.run(new Random(seed));
 
 		System.setOut(oldOut);
-		Approvals.verify(outputStream.toString());
+		return outputStream.toString();
 	}
 }
